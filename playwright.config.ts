1b43975d7152b8a50077ac896d2e9871cb49c0e3
darshan-chavda3 <dhchavda1931@gmail.com>
2025-03-09 19:04:31 +0530

    restructure pom

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,58 +1,33 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  timeout: 90 * 1000,
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [["list"], ["html", {open: "on-failure"}]],
+    testDir: './src/tests',
+    timeout: 90 * 1000,
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: [['list'], ['html', { open: 'on-failure' }]],
 
-  use: {
-    baseURL: 'https://www.saucedemo.com',
-    headless: true,
-    actionTimeout: 90 * 1000,
-    navigationTimeout: 90 * 1000,
-    trace: 'on-first-retry',
-  },
-
-  projects: [
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    use: {
+        baseURL: 'https://www.saucedemo.com',
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        actionTimeout: 90 * 1000,
+        navigationTimeout: 90 * 1000,
+        trace: 'on-first-retry',
     },
 
-    /*
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
-
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    */
-  ],
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
+    ],
 });
